d3f02715de76e5544bce50ec985fab36a10717c1
kassiosilva <kassio.silva@ufersa.edu.br>
2022-08-19 10:00:03 -0300

    To heroku

---
+++ init.R
@@ -0,0 +1,16 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+my_packages = c("readxl", "tidyr", "dplyr", "readxl", "ggplot2",
+                "reshape2", "shiny", "shinydashboard")
+
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(my_packages, install_if_missing))
